df89da1e72afb2abfe655a80a30e1a38310771f4
imgul <bc210411296mgu@vu.edu.pk>
2022-08-27 23:41:06 +0500

    add sass boilerplate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1>Gulzaib</h1>
     </div>
   );
 }
